47bc976cc2fe9698408530ee4f7be0c16471de7a
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-07-21 19:15:18 +0000

    Add decrypted function and check user's action

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+#encrypt
 def encrypt(text, shift):
   text_list = list(text)
   text_shifted = ''
@@ -11,8 +12,23 @@ def encrypt(text, shift):
   for i in range(len(text_list)):
     #loop each alphabet + shift --> return shift alphabet from alphabet[default + shift]
     text_shifted += alphabet[alphabet.index(text_list[i]) + shift]
-  #print ciphet text that already shifted
+  #print cipher text that already shifted
   print(f"The encoded text is {text_shifted}")
-#triggered function
-encrypt(text, shift)
 
+#decrypt
+def decrypt(text, shift):
+  text_list = list(text)
+  text_decrypted = ''
+
+  for letter in text_list:
+    #the same logic as encrypted but different way to code
+    old_position = alphabet.index(letter)
+    text_decrypted += alphabet[old_position - shift]
+  #print decrypt text that already unshifted
+  print(f"The decoded text is {text_decrypted}")
+
+#Check what user wanted
+if direction == "encode":
+  encrypt(text, shift)
+else:
+  decrypt(text, shift)
